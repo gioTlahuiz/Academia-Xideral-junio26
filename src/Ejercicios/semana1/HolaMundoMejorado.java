7d5a30bab197ef75a8bbc91b5067ae7efe54aef2
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-06-03 13:42:50 -0600

    ejercicio 1 resuelto

--- src/Ejercicios/semana1/HolaMundoMejorado.java
+++ src/Ejercicios/semana1/HolaMundoMejorado.java
@@ -1,4 +1,24 @@
 package Ejercicios.semana1;
 
 public class HolaMundoMejorado {
+    public static void main(String[] args) {
+        // TODO: Declarar variables
+        String nombre = "Giovanni";
+        int edad = 25;
+        double altura = 1.76;
+        boolean esActivo = true;
+
+        // TODO: Concatenacion con +
+        String mensaje1 = "Me llamo " + nombre + ", tengo " + edad
+                + " anios, mido " + altura + "m y estoy "
+                + (esActivo ? "activo" : "inactivo") + ".";
+        System.out.println(mensaje1);
+
+        // TODO: Usando String.format()
+        String mensaje2 = String.format(
+                "Me llamo %s, tengo %d anios, mido %.2f m y estoy %s.",
+                nombre, edad, altura, esActivo ? "activo" : "inactivo"
+        );
+        System.out.println(mensaje2);
+    }
 }
