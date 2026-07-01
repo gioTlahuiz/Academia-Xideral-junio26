6255bddaf4750e455b4ede56398d4592a98ae8c2
gioTlahuiz <fcei.tlahuiz.2.e@gmail.com>
2026-07-01 12:41:07 -0600

    se agrego el archivo para ejecutar la aplicacion.

---
+++ Proyecto con Copilot/src/main/java/com/academia/batch/SpringBatchFinalCalificacionesApplication.java
@@ -0,0 +1,12 @@
+package com.academia.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFinalCalificacionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchFinalCalificacionesApplication.class, args);
+    }
+}
